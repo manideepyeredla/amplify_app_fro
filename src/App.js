eec0afc4043c7ba83214780a95b5f65be65ba9d1
manideepyeredla <163824047+manideepyeredla@users.noreply.github.com>
2024-08-31 00:35:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import QueryForm from './components/QueryForm';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(awsconfig);
 
 const App = () => {
   return (
-    <div>
-      <h1>Fraud Transaction Data</h1>
-      <QueryForm/>
-    </div>
+    <Authenticator signUpConfig={{ hiddenDefaults: ['email'] }} hideSignUp>
+      {({ signOut, user }) => (
+        <div>
+          <h1>Fraud Transaction Data</h1>
+          <p>Welcome, {user.username}</p>
+          <QueryForm />
+          <button onClick={signOut}>Sign out</button>
+        </div>
+      )}
+    </Authenticator>
   );
 };
 
